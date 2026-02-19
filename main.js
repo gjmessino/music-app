2a4377de7b5add41ee67efbe7aa1abe22bf3f50f
gjmessino <gabyjmessino@gmail.com>
2026-02-19 13:22:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const CLIENT_ID = //
 const CLIENT_SECRET = //
+const token = getToken();
 
 async function getToken(){
     let authString = btoa(CLIENT_ID + ":" + CLIENT_SECRET);
@@ -13,4 +14,14 @@ async function getToken(){
     });
     const data = await result.json();
     return data.access_token;
+}
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
 }
