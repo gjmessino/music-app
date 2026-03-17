9b91a47069d4b12c6e6875c75ddbca059b3db0bc
gjmessino <gabyjmessino@gmail.com>
2026-03-16 17:14:45 -0700

    idk what im doing any more

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
-const CLIENT_ID = //
-const CLIENT_SECRET = //
+//const CLIENT_ID = //
+//const CLIENT_SECRET = //
 const token = getToken();
 
-async function getToken(){
-    let authString = btoa(CLIENT_ID + ":" + CLIENT_SECRET);
-    const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": "Basic " + authString
-        },
-        body: "grant_type=client_credentials"
-    });
-    const data = await result.json();
-    return data.access_token;
+async function getProfile(accessToken) {
+  let accessToken = localStorage.getItem('access_token');
+  const response = await fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: 'Bearer ' + accessToken
+    }
+  });
+  const data = await response.json();
 }
+
+//async function getToken(){
+//    let authString = btoa(CLIENT_ID + ":" + CLIENT_SECRET);
+//    const result = await fetch("https://accounts.spotify.com/api/token", {
+ //       method: "POST",
+ //       headers: {
+ //           "Content-Type": "application/x-www-form-urlencoded",
+ //           "Authorization": "Basic " + authString
+ //       },
+ //       body: "grant_type=client_credentials"
+ //   });
+//    const data = await result.json();
+//    return data.access_token;
+//}
 async function fetchWebApi(endpoint, method, body) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
     headers: {
@@ -33,7 +43,7 @@ async function getMyArtists() {
     title_element.innerHTML = title
 }
 
-async function getMyTracksA() {
+async function getMyTracks() {
     const data = await fetchWebApi(`v1/me/top/tracks`, "GET");
     const title = String(data.data.tracks.id)
     const title_element = document.getElementById("track_name")
